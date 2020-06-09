32e8351738f459baef81a5e81545fd8307fd61e0
normahanim98 <66402177+normahanim98@users.noreply.github.com>
2020-06-09 20:01:27 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -13,4 +13,23 @@
      die ("Error connection: ".sqlsrv_errors());
      }
      echo "DB Server: Connected!";
+     $tsql= "SELECT*FROM [dbo].[restaurant]";
+     $getResults = sqlsrv_query($conn, $tsql);
+     if ($getResults == FALSE)
+     {
+       die(sqlsrv_errors());
+     }
+     echo"<table border=’1’>";
+     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+     {
+        echo "<tr>";
+        echo "<td>".$row['restaurant_id']. "</td>";
+        echo "<td>".$row['restaurant_name']. "</td>";
+        echo "<td>".$row['restaurant_address']. "</td>";
+        echo "<td>".$row['restaurant_phone']. "</td>";
+        echo "</tr>";
+     }
+     echo"</table>";
+     Sqlsrv_free_stmt($getResults);
+
 ?>
