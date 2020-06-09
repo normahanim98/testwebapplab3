2ac51ecf55f6d7373afbc89e5f3a0d52742ba043
normahanim98 <66402177+normahanim98@users.noreply.github.com>
2020-06-09 19:38:43 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -4,13 +4,13 @@
     $connectionOptions = array(
           “Database” => “testdbtp043719”,
           “Uid” => “apuadmin”,
-           “PWD” => “apu@123456”);  //apu@123456
+          “PWD” => “apu@123456”);  
     //Establish the connection
     $conn = sqlsrv_connect ($serverName, $connectionOptions);
     If (!$conn)
     {
      die (“Error connection: “.sqlsrv_errors());
      }
-     Echo “DB Server: Connected!”;
-}
+     echo “DB Server: Connected!”;
+    
 ?>
